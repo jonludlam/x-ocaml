b5adae06b9ac8ecbaa3899db8639f40b37e87d6f
Jon Ludlam <jon@recoil.org>
2025-09-29 12:08:45 +0100

    minor tweaks

--- src/webcomponent.mli
+++ src/webcomponent.mli
@@ -4,3 +4,4 @@ val define : Jstr.t -> (t -> unit) -> unit
 val text_content : t -> string
 val as_target : t -> Brr.El.t
 val attach_shadow : t -> Brr.El.t
+val get_attribute : t -> string -> Jstr.t option
